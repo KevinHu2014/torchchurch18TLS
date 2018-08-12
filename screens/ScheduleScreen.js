7c36e6e32f744c00eab8a90d36a203a280f6d3e4
KevinHu2014 <kevinhu@weco.net>
2018-08-12 23:52:17 +0800

    實作抓取json schedule 資料

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet, View, ScrollView,
+  Platform,
+} from 'react-native';
+import { ListSection, ListItem } from 'react-native-paper';
+import axios from 'axios';
 import AnimatedTopTabs from '../components/AnimatedTopTabs';
-import TopTabs from '../components/TopTabs';
-
-const DATA = ['Top Tab 1 Content', 'Extra Stuff for Top Tab 2', 'More stuff for Top Tab 3'];
 
 const styles = StyleSheet.create({
   container: {
@@ -15,13 +17,55 @@ export default class ScheduleScreen extends React.Component {
     super(props);
     this.state = {
       currentTab: 0,
+      Data: null,
     };
+  }
+
+  componentDidMount() {
+    const url = 'https://gist.githubusercontent.com/KevinHu2014/bb2f9e96dba15f6bd26804874f3ff0a9/raw/b54dba85e9e587e7a3ac795093ad673ed57a22f5/tlsschedule.json';
+    axios.get(url)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ Data: response.data });
+      })
+      .catch((err) => { console.log(err); });
   }
 
   handleTabChange = value => this.setState({ currentTab: value });
 
+  renderContent() {
+    const { currentTab, Data } = this.state;
+    if (!Data) {
+      return true;
+    }
+    return Data[`Day${currentTab + 1}`].map((events) => {
+      const time = new Date(events.start);
+      const diff = (new Date(events.end) - new Date(events.start)) / 1000;
+      const Hour = diff / (60 * 60);
+      const Min = (diff % (60 * 60)) / 60;
+      const duration = `${(Hour >= 1) ? Math.floor(Hour) : ''}${(Hour >= 1) ? ' HOUR ' : ''}${(Min >= 1) ? Min : ''}${(Min >= 1) ? ' MIN' : ''}`;
+      return (
+        <View key={events.start}>
+          <ListSection
+            title={
+              Platform.OS === 'ios'
+                ? time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
+                : time.toLocaleTimeString().substr(0, 5)
+            }
+            style={{ backgroundColor: '#f3f3f3' }}
+          >
+            <ListItem
+              style={{ backgroundColor: '#fff' }}
+              title={events.name}
+              description={`${events.speaker ? events.speaker : ''}${events.speaker ? '- ' : ''}${duration}`}
+            />
+          </ListSection>
+        </View>
+      );
+    });
+  }
+
   render() {
-    const { currentTab } = this.state;
     return (
       <View style={styles.container}>
         <AnimatedTopTabs
@@ -31,7 +75,9 @@ export default class ScheduleScreen extends React.Component {
           tabTitles={['Day 1', 'Day 2', 'Day 3']}
           onChangeTab={this.handleTabChange}
         />
-        <TopTabs />
+        <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
+          {this.renderContent()}
+        </ScrollView>
       </View>
     );
   }
