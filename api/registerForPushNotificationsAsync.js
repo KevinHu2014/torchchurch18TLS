9b833021ae8c8618cb6ddd3ffe9e95fe71ee6b5d
KevinHu2014 <kevinhu@weco.net>
2018-08-25 03:16:28 +0800

    Firebase 推播token

--- api/registerForPushNotificationsAsync.js
+++ api/registerForPushNotificationsAsync.js
@@ -1,9 +1,39 @@
+/* eslint-disable  consistent-return  */
 import { Constants, Permissions, Notifications } from 'expo';
+import * as firebase from 'firebase';
+import firebaseConfig from '../firebaseConfig';
 
 // Example server, implemented in Rails: https://git.io/vKHKv
 const PUSH_ENDPOINT = 'https://expo-push-server.herokuapp.com/tokens';
 
 export default (async function registerForPushNotificationsAsync() {
+  let userUID = null;
+  console.log('heeellllllllllo world');
+  // firebase init
+  await firebase.initializeApp(firebaseConfig);
+  // firebase signInAnonymously
+  await firebase.auth().signInAnonymously().catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+  });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in.
+      const { uid } = user;
+      userUID = uid;
+      console.log(uid);
+      // ...
+    } else {
+      // User is signed out.
+      console.log('User is signed out');
+      // ...
+    }
+    // ...
+  });
+
   // Remote notifications do not work in simulators, only on device
   if (!Constants.isDevice) {
     return;
@@ -22,6 +52,13 @@ export default (async function registerForPushNotificationsAsync() {
   const token = await Notifications.getExpoPushTokenAsync();
   console.log('token >>>>>>>>>>>');
   console.log(token);
+
+  // clear warning
+  // https://github.com/facebook/react-native/issues/12981
+  console.ignoredYellowBox = ['Setting a timer'];
+  firebase.database().ref(`users/${userUID}`).set({
+    notification: token,
+  });
   // POST the token to our backend so we can use it to send pushes from there
   return fetch(PUSH_ENDPOINT, {
     method: 'POST',
