a48fedce29573d60ef510a33a78354dbc476c5f7
KevinHu2014 <kevinhu@weco.net>
2018-08-25 22:09:51 +0800

    收件人名稱改成寄給火把領袖 並且把字體放大

--- screens/information/PreNotificationScreen.js
+++ screens/information/PreNotificationScreen.js
@@ -18,6 +18,12 @@ const styles = StyleSheet.create({
     height: 40,
     width: 40,
   },
+  contentStyle: {
+    paddingLeft: 15,
+    paddingBottom: 30,
+    fontSize: 17,
+    lineHeight: 25,
+  },
 });
 
 export default class PreNotification extends React.Component {
@@ -27,7 +33,7 @@ export default class PreNotification extends React.Component {
         <Card>
           <ListItem
             title="火把教會"
-            description={'寄給 火把人\n 下午10:09'}
+            description={'寄給 火把領袖\n 下午10:09'}
             avatar={(
               <Image
                 source={require('../../assets/images/email-icon.png')} // eslint-disable-line
@@ -35,9 +41,7 @@ export default class PreNotification extends React.Component {
               />
             )}
           />
-          <Paragraph
-            style={{ paddingLeft: 15, paddingBottom: 30 }}
-          >
+          <Paragraph style={styles.contentStyle}>
             {'您好！\n2018火把領袖高峰會即將展開！\n我們非常期待與您在高峰會中相會！\n\n以下事項請特別留意：\n\n本次領袖高峰會將於09/07(五)19:00展開，\n當天的報到時間為17:30-19:50\n報到的地點為臺鐵六樓演藝廳 東側入口處。\n(原銷售櫃檯)\n請您告知櫃檯同工您的姓名，\n以利同工確認報名資訊並發取物資包給您。\n\n若您是09/08(六)才能前來參加領袖高峰會，\n當天同樣有報到櫃檯為您服務報到，\n時間為08:30-09:50。\n祝您在領袖高峰會大得造就、經歷神的擴張。\n\nbest regards,\n火把教會'}
           </Paragraph>
         </Card>
