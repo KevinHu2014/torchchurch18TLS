e57e8e4daccd9f333db8ada10c18467eaa87f00f
KevinHu2014 <kevinhu@weco.net>
2018-08-26 19:30:21 +0800

    更換banner style

--- screens/InformationScreen.js
+++ screens/InformationScreen.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Platform, View } from 'react-native';
-import { WebBrowser } from 'expo';
-import { ListItem, ListSection, Divider, Paragraph } from 'react-native-paper';
+import {
+  ScrollView,
+  StyleSheet,
+  Platform,
+  View,
+  Text,
+} from 'react-native';
+import { WebBrowser, LinearGradient } from 'expo';
+import { ListItem, ListSection, Divider } from 'react-native-paper';
 import PropTypes from 'prop-types';
 import Colors from '../constants/Colors';
 
@@ -12,16 +18,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   topStyle: {
-    height: 80,
-    backgroundColor: Colors.tabBar,
-    color: Colors.header,
-    borderColor: Colors.header,
-    borderWidth: 2,
-    margin: 5,
-    marginTop: 6,
-    marginBottom: 6,
-    padding: 5,
+    backgroundColor: Colors.header,
+    color: Colors.headerText,
+    margin: 3,
     fontSize: 13,
+    lineHeight: 17,
+    flex: 1,
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontWeight: '200',
   },
 });
 export default class InformationScreen extends React.Component {
@@ -29,10 +35,21 @@ export default class InformationScreen extends React.Component {
     const { navigation } = this.props;
     return (
       <ScrollView style={styles.container}>
-        <View style={{ backgroundColor: Colors.tabBar, justifyContent: 'center' }}>
-          <Paragraph style={styles.topStyle}>
-            {'以賽亞書54:2\n要擴張你帳幕之地，張大你居所的幔子，不要限止；\n要放長你的繩子，堅固你的橛子。 '}
-          </Paragraph>
+        <View style={{ backgroundColor: Colors.header, justifyContent: 'center' }}>
+          <LinearGradient
+            colors={['#E0C98D', '#AC8331', '#C2B171']}
+            start={{ x: 0, y: 1 }}
+            end={{ x: 1, y: 1 }}
+            locations={[0, 0.7, 0.9]}
+            style={{
+              height: 100,
+              margin: 3,
+            }}
+          >
+            <Text style={styles.topStyle}>
+              {'以賽亞書54:2\n\n要擴張你帳幕之地，張大你居所的幔子，不要限止；\n要放長你的繩子，堅固你的橛子。 '}
+            </Text>
+          </LinearGradient>
         </View>
         <ListSection title="活動資訊">
           <ListItem
